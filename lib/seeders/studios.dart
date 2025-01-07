cc36f1472fc6205d8344ab8d532689632d833300
Kevin <corneliuskevin770@gmail.com>
2025-01-07 11:44:50 +0700

    ganti seat studio jadi 64

--- lib/seeders/studios.dart
+++ lib/seeders/studios.dart
@@ -3,19 +3,19 @@ import 'package:sqflite/sqflite.dart';
 Future<void> seedStudios(Batch batch) async {
   batch.insert('studios', {
     'name': 'Studio 1',
-    'seat': 40,
+    'seat': 64,
     'created_at': DateTime.now().toString(),
     'updated_at': DateTime.now().toString(),
   });
   batch.insert('studios', {
     'name': 'Studio 2',
-    'seat': 48,
+    'seat': 64,
     'created_at': DateTime.now().toString(),
     'updated_at': DateTime.now().toString(),
   });
   batch.insert('studios', {
     'name': 'Studio 3',
-    'seat': 56,
+    'seat': 64,
     'created_at': DateTime.now().toString(),
     'updated_at': DateTime.now().toString(),
   });
